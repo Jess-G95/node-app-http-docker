5de1bd9c5e02f1aa5fb6af29e89135220be69976
Jess-G95 <79566501+Jess-G95@users.noreply.github.com>
2023-03-29 19:59:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ const server = http.createServer(async (req, res) => {
     else if (req.url === "/api/trace" && req.method === "POST") {
         // get the data sent along
         let todo_data = await getReqData(req);
-        console.log(JSON.parse(todo_data));
+        console.log(JSON.stringify(todo_data, null, 4));
         // set the status code and content-type
         res.writeHead(200, { "Content-Type": "application/json" }); //was 200
     }
