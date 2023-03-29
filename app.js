2c3f078aa1757c005acf6a08ae1f190bb2bdd6f9
Jess-G95 <79566501+Jess-G95@users.noreply.github.com>
2023-03-29 20:06:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -126,6 +126,7 @@ const server = http.createServer(async (req, res) => {
         let todo_data = await getReqData(req);
         // create the todo
         console.log(JSON.stringify(JSON.parse(todo_data), null, 2));
+        console.log("come on");
         // set the status code and content-type
         res.writeHead(200, { "Content-Type": "application/json" }); //was 200
     }
